f4a4b8a44bfc5d7dc18716336011b1b8ddfed3e6
BakaNoCrimson <Sidney.nguyen@live.fr>
2024-07-02 14:36:50 +0200

    jest upload

---
+++ src/exercises/ex0.js
@@ -0,0 +1,9 @@
+function fizz_buzz(string, substring) {
+    if(string.includes(substring) == true) {
+        return true;
+    }else{
+        return false;
+    }
+}
+console.log("aaa");
+module.exports = fizz_buzz;
---
+++ src/exercises/ex0.test.js
@@ -0,0 +1,13 @@
+const fizz_buzz = require('./ex0');
+
+describe("FizzBuzz", () => {
+  test('[3] should result in "fizz"', () => {
+    expect(fizz_buzz("aaa", "a")).toBe(true);
+  });
+  test('[3] should result in "fizz"', () => {
+    expect(fizz_buzz("aaa", "b")).toBe(false);
+  });
+
+   
+
+});
